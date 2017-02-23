bfd208cb6240a4512d51104294497d7ba097ec68
Til Blechschmidt <til@blechschmidt.de>
2017-02-23 17:17:32 +0100

    Added index.html.gz file to repo and ability to skip webpack

--- handleUpload.py
+++ handleUpload.py
@@ -1,9 +1,12 @@
 # from subprocess import call
 import subprocess
+import os
 
 Import("env")
 
 def before_spiffs(source, target, env):
+    if os.environ.get('SKIP_WEBPACK'):
+        return
     print("Running webpack...")
     p = subprocess.Popen('cd http && ./buildProduction.sh', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
--- http/buildProduction.sh
+++ http/buildProduction.sh
@@ -1,5 +1,6 @@
+DEST="../data/"
+
 npm i
 NODE_ENV="production" ./node_modules/webpack/bin/webpack.js --hide-modules -p
-mkdir -p dist/SPIFFS
-gzip -9 < dist/index.html > dist/SPIFFS/index.html.gz
-#cp dist/index.html dist/SPIFFS/
+mkdir -p ${DEST}
+gzip -9 < dist/index.html > "${DEST}index.html.gz"
