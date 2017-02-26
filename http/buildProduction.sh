8a0b1fbfd6fe9f74bcc13ce04b12843d63e3b44e
Til Blechschmidt <til@blechschmidt.de>
2017-02-26 23:48:39 +0100

    Moved wificonfig file directly to data dir

--- http/buildProduction.sh
+++ http/buildProduction.sh
@@ -5,5 +5,3 @@ npm i
 NODE_ENV="production" ./node_modules/webpack/bin/webpack.js --hide-modules -p
 mkdir -p ${DEST}
 gzip -9 < dist/index.html > "${DEST}index.html.gz"
-
-cp wificonfig.html ${DEST}
